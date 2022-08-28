71a0b575e8d8c4da0d83f30cc2f14e332f6940a6
dlavrukhin <dlavrukhin@yandex.ru>
2022-08-28 17:09:25 +0300

    small refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 import logging
 
-from telegram import (
-    ForceReply,
-    Update,
-)
 from telegram.ext import (
-    CallbackContext,
     CommandHandler,
     Filters,
     MessageHandler,
     Updater,
 )
 
-from app import settings
-
+from app import (
+    commands,
+    settings,
+)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -22,31 +19,15 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Help!')
-
-
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
-
-
 def main() -> None:
     updater = Updater(settings.BOT_TOKEN)
 
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("start", commands.start))
+    dispatcher.add_handler(CommandHandler("help", commands.help_command))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, commands.echo))
 
     updater.start_polling()
     updater.idle()
---
+++ app/commands.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from telegram import Update
+from telegram.ext import CallbackContext
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+    )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
